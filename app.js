4b5d5ad56af065364fcd10a5de4e2d2fee7c8b8e
Nic2003Vod <nicodemevodounnou90@gmail.com>
2023-03-22 16:07:18 +0100

    traitement du formulaire d'inscription et envoie des données dans une base de données mysql

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import express from "express";
 import mysql from "mysql";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 8000;
+app.set('view engine','ejs');
+app.use(express.static('public'));
 
 //Configuration de la base de donnée
 const con = mysql.createConnection({
@@ -16,10 +22,9 @@ con.connect((error)=>{
     }
     console.log("Connexion à la base de donnée réussi !");
 });
-const app = express();
-const port = 8000;
-app.set('view engine','ejs');
-app.use(express.static('public'));
+
+// Middleware pour parser les données du formulaire
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Affichage de la page d'accueil
 app.get("/",(request,response)=>{
@@ -29,13 +34,26 @@ app.get("/",(request,response)=>{
 app.get("/login",(request,response)=>{
    response.render('login.ejs');
 });
+
 //Affichage de la page d'inscription
 app.get("/register", (request,response)=>{
     response.render('signup.ejs');
 });
 
 //Traitement du formulaire d'inscription
-
+app.post("/regtrait",(request,response)=>{
+    const { nom, prenom, email, pass } = request.body;
+    const mdpc = request.body.cpass;
+    if (pass == mdpc)
+    {
+        const sql = `INSERT INTO users(nom, prenom, email, mdp) VALUES ('${nom}', '${prenom}', '${email}', '${pass}')`;
+        con.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            response.redirect('/login');
+        });
+    }
+});
 app.listen(port,()=>{
    console.log(`Serveur http://localhost:${port} en cours`);
 });
